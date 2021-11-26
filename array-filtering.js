590532d035267f951c6468473e1861be1e09bdbb
Karolina Redden <dubajkaro@gmail.com>
2021-11-26 01:16:15 +0000

    Initial commit for javascripting task

---
+++ array-filtering.js
@@ -0,0 +1,30 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const filtered = numbers.filter(function evenNumbers (number) {  
+  return number % 2 === 0  
+  })
+console.log(filtered)
+
+
+/*
+In that file, define a variable named numbers that references 
+this array:  
+   
+     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+   
+  Like above, define a variable named filtered that references 
+  the result of  numbers.filter().  
+   
+  The function that you pass to the .filter() method will look 
+  something  like this:  
+   
+     function evenNumbers (number) {  
+       return number % 2 === 0  
+     }  
+   
+  Pay close attention to the syntax used throughout your solution. Use  
+  console.log() to print the filtered array to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify array-filtering.js
+     */
---
+++ looping-through-arrays.js
@@ -0,0 +1,14 @@
+pets = ['cat', 'dog', 'rat'];
+for (i=0; i<pets.length; i++ )
+{ 
+pets[i] = pets[i] + 's';
+}
+console.log(pets)
+
+/* a diffrent loop
+let total = 0
+let limit = 10
+for (i = 0; i < limit; i++ ) {
+  total += i }
+console.log(total)
+*/
