a3b02de071134d305700e00d35b65bf7b13b1d50
Karolina Redden <dubajkaro@gmail.com>
2021-12-27 11:27:14 +0000

    javascripting part2

---
+++ functions.js
@@ -0,0 +1,20 @@
+
+ function eat(food) {  
+  return food + ' tasted really good.' 
+} 
+console.log(eat("bananas"))
+
+
+// In that file, define a function named eat that takes an argument named  
+// food that is expected to be a string.  
+ 
+// Inside the function return the food argument like this:  
+ 
+//    return food + ' tasted really good.'  
+ 
+// Inside of the parentheses of console.log(), call the eat() function with  
+// the string bananas as the argument.  
+ 
+// Check to see if your program is correct by running this command:  
+ 
+//    javascripting verify functions.js  
